684ae9beacf63d0f1a2cfff3a6f6bb476bff2d4d
vamsi-500apps <vamsi.ajjumpudi@500apps.com>
2022-12-15 19:42:51 +0530

    Assignment

---
+++ task17/script.js
@@ -0,0 +1,13 @@
+function alphabeticalOrder(arr) {
+    // Only change code below this line
+  
+    return arr.sort();
+    // Only change code above this line
+  }
+  
+  alphabeticalOrder(["a", "d", "c", "a", "z","g"]);
+
+
+  function getOutput() {
+    document.getElementById("output").innerText=alphabeticalOrder(["a", "d", "c", "a", "z","g"]);
+  }
---
+++ task5/script.js
@@ -0,0 +1,16 @@
+// The global variable
+let fixedValue = 4;
+let a=fixedValue
+
+// Only change code below this line
+function incrementer(a) {
+  return a+1
+
+
+  // Only change code above this line
+}
+incrementer(a)
+function getOutput() {
+    document.getElementById("output").innerText=incrementer(fixedValue);
+
+}
